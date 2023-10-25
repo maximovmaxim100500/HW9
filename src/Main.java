cb7788bc54e3e068a96c3455b5a92f911c6a18eb
MaximovMaxim <maximovmaxim100500@gmail.com>
2023-10-25 23:23:47 +0300

    Домашка готова

--- src/Main.java
+++ src/Main.java
@@ -46,6 +46,6 @@ public class Main {
         char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
         for (int i = reverseFullName.length - 1; i >= 0; i--) {
             System.out.print(reverseFullName[i]);
-        }
+        }//
     }
 }
